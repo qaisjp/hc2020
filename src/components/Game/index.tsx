b0945cf2dc7dd6b2692dd0acd5d9b26fa75b9140
Qais Patankar <qaisjp@gmail.com>
2020-01-18 14:31:43 +0000

    add phaser

--- src/components/Game/index.tsx
+++ src/components/Game/index.tsx
@@ -1,7 +1,42 @@
-import React from 'react'
+import React, {useRef} from 'react'
+import Phaser from "phaser";
+import logoImg from "../../imgs/img.jpg";
+
+function preload() {
+  this.load.image("logo", logoImg);
+}
+
+function create() {
+  const logo = this.add.image(400, 150, "logo");
+
+  this.tweens.add({
+    targets: logo,
+    y: 450,
+    duration: 2000,
+    ease: "Power2",
+    yoyo: true,
+    loop: -1
+  });
+}
+
 
 const Game = () => {
-    return <p>Game</p>;
+    const ref = useRef();
+
+    const config = {
+        type: Phaser.AUTO,
+        parent: "phaser-example",
+        width: 800,
+        height: 600,
+        scene: {
+        preload: preload,
+        create: create
+        }
+    };
+
+    const game = new Phaser.Game(config);
+
+    return <div id="phaser-example"></div>;
 }
 
 export default Game
