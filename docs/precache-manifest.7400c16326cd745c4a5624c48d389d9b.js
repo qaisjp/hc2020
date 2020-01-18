4a8adc3e31ef0b6f6eb0e019309915a0f16864f7
Qais Patankar <qaisjp@gmail.com>
2020-01-18 13:57:43 +0000

    add docs folder

---
+++ docs/precache-manifest.7400c16326cd745c4a5624c48d389d9b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "922985b8205dc595ba7c7b1daffa1cf7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "33542c7d488b3412c90b",
+    "url": "/static/css/main.a0d18ae2.chunk.css"
+  },
+  {
+    "revision": "4d71430fd82605e6376f",
+    "url": "/static/js/2.57cd51d9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.57cd51d9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "33542c7d488b3412c90b",
+    "url": "/static/js/main.3fd5113e.chunk.js"
+  },
+  {
+    "revision": "cf84660092701a43f022",
+    "url": "/static/js/runtime-main.1c7f7e1e.js"
+  }
+]);
